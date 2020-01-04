4aac8b3d9625e8f6ffd275e14b63845d63240141
Delkys Welffer <delkys.yo@gmail.com>
2020-01-04 13:45:53 -0400

    added Login class: a base class for the User login and Admin login to save code doing all the heavy stuff in this base class and only composing the model, the register view and the home view for that model in the login subclassing this class. modified Admin class: now it uses the Login class as its base class. added User class: it uses Login as its base class. it serves as the login view for the users.

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from tkinter import *
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mogli.settings')
 django.setup()
 from mogli.models import Admin as AdminModel
+from mogli.models import User as UserModel
 
 session = []
 
 LARGE_FONT = ("Verdana", 12)
 
 
+# General views and stuff
 class Application(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -28,7 +30,7 @@ class Application(tk.Tk):
         self.title("Fraud detection System")
         self.frames = {}
 
-        for F in (MainPage, Admin, AdminRegister, AdminWindow):
+        for F in (MainPage, Admin, AdminRegister, AdminWindow, User):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -48,12 +50,82 @@ class MainPage(tk.Frame):
         label.pack(pady=10, padx=10)
 
         button = tk.Button(self, text="User", height="2", width="30",
-            command=lambda: controller.show_frame(PageOne))
+            command=lambda: controller.show_frame(User))
         button.pack(padx=5, pady=5)
 
         button2 = tk.Button(self, text="Admin", height="2", width="30",
             command=lambda: controller.show_frame(Admin))
         button2.pack(padx=5, pady=5)
+
+# General abstract login view
+class Login(tk.Frame):
+
+    def __init__(self, parent, controller, type, model, register_view, return_view):
+        tk.Frame.__init__(self, parent)
+        
+        
+        self.username = StringVar()
+        self.password = StringVar()
+        self.controller = controller
+        self.type = type
+        self.model = model
+        self.register_view = register_view
+        self.return_view = return_view
+
+        label = Label(self, text=f"Login {self.type}", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        username_label = Label(self, text="Username *", width="20")
+        username_label.pack()
+
+        self.username_entry = Entry(self, textvariable=self.username,
+            width="20")
+        self.username_entry.pack()
+
+        password_label = Label(self, text="Password *", width="20")
+        password_label.pack()
+
+        self.password_entry = Entry(self, textvariable=self.password, 
+            show="*", width="20")
+        self.password_entry.pack()
+
+        self.info_label = Label(self, text="")
+        self.info_label.pack()
+
+        login_btn = Button(self, text="Login", height="1", width="17", 
+            command=self.login)
+        login_btn.pack()
+
+
+        mainPage_btn = Button(self, text="Back to Home",
+            command=lambda: controller.show_frame(MainPage))
+        mainPage_btn.pack(side=tk.RIGHT, padx=5, pady=5)
+
+        register_btn = Button(self, text="Register",
+            command=lambda: controller.show_frame(self.register_view))
+        register_btn.pack(side=tk.RIGHT)
+
+    def login(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        if username:
+            try:
+                target = self.model.objects.get(username=username)
+                if target.password == password:
+                    print("Login")
+                    session.append((username, 1))
+                    self.controller.show_frame(self.return_view)
+                else:
+                    print("Password incorrect")
+
+            except mogli.models.DoesNotExist:
+                print("Admin doesn't exist")
+        else:
+            print(f"Field username empty")
+
+        self.username_entry.delete(0, END)
+        self.password_entry.delete(0, END)
+        print(f"Login: {username} - {password}")
 
 # Admin views
 class AdminWindow(tk.Frame):
@@ -84,69 +156,10 @@ class AdminWindow(tk.Frame):
         button.pack()
 
 
-class Admin(tk.Frame):
+class Admin(Login):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = Label(self, text="Login Admin", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-        
-        self.username = StringVar()
-        self.password = StringVar()
-        self.controller = controller
-
-        username_label = Label(self, text="Username *", width="20")
-        username_label.pack()
-
-        self.username_entry = Entry(self, textvariable=self.username,
-            width="20")
-        self.username_entry.pack()
-
-        password_label = Label(self, text="Password *", width="20")
-        password_label.pack()
-
-        self.password_entry = Entry(self, textvariable=self.password, 
-            show="*", width="20")
-        self.password_entry.pack()
-
-        self.info_label = Label(self, text="")
-        self.info_label.pack()
-
-        login_btn = Button(self, text="Login", height="1", width="17", 
-            command=self.login)
-        login_btn.pack()
-
-
-        mainPage_btn = Button(self, text="Back to Home",
-            command=lambda: controller.show_frame(MainPage))
-        mainPage_btn.pack(side=tk.RIGHT, padx=5, pady=5)
-
-        register_btn = Button(self, text="Register",
-            command=lambda: controller.show_frame(AdminRegister))
-        register_btn.pack(side=tk.RIGHT)
-
-    def login(self):
-        username = self.username_entry.get()
-        password = self.password_entry.get()
-
-        if username:
-            try:
-                admin = AdminModel.objects.get(username=username)
-                if admin.password == password:
-                    print("Login")
-                    session.append((username, 1))
-                    self.controller.show_frame(AdminWindow)
-                else:
-                    print("Password incorrect")
-
-            except mogli.models.DoesNotExist as e:
-                print("Admin doesn't exist")
-        else:
-            print(f"Field username empty")
-
-        self.username_entry.delete(0, END)
-        self.password_entry.delete(0, END)
-        print(f"Login: {username} - {password}")
+        super().__init__(parent, controller, "Admin", AdminModel, AdminRegister, AdminWindow)
 
 
 class AdminRegister(tk.Frame):
@@ -197,6 +210,27 @@ class AdminRegister(tk.Frame):
             print("Empty fields*")
 
         print(f"{username} - {password}")
+
+
+# User views
+class User(Login):
+
+    def __init__(self, parent, controller):
+
+        super().__init__(parent, controller, "User", UserModel, UserRegister, UserWindow)
+
+
+class UserRegister(tk.Frame):
+
+    def __init__(self, parent, controller):
+        pass
+
+
+class UserWindow(tk.Frame):
+
+    def __init__(self, parent, controller):
+        pass
+
 
 
 if __name__ == '__main__':
